f5222950409261c267abbfbe77e0bbb3ca55698b
binghuan <binghuanlin@gmail.com>
2017-11-28 16:38:39 +0800

    beautify codes.

--- fibonacciNumbers.js
+++ fibonacciNumbers.js
@@ -1,4 +1,8 @@
-let fibonacciNumbers = function(numberLength) {
+/*
+    Page: https://en.wikipedia.org/wiki/Fibonacci_number
+*/
+
+let fibonacciNumbers = function (numberLength) {
 
     let number = 1;
 
@@ -8,13 +12,13 @@ let fibonacciNumbers = function(numberLength) {
     numArray.push(number);
 
     // main function. 
-    for(let i =0; i< numberLength; i++) {
+    for (let i = 0; i < numberLength; i++) {
 
         let lastOne = numArray.pop();
         let lastTwo = numArray.pop();
         let sum = 0;
 
-        if(lastTwo != null) {
+        if (lastTwo != null) {
             sum = lastOne + lastTwo;
             numArray.push(lastTwo);
         } else {
--- showMatrixByColumnRowSet.js
+++ showMatrixByColumnRowSet.js
@@ -1,28 +1,26 @@
-let showMatrix = function(columnCount, rowCount) {
-    
-        // show numbers from 1 to N 
-        // for instance: 1 ~  9
-        /*
-            1,2,3
-            4,5,6
-            7,8,9
-        */
-    
-        let number = 1;
-        let totalCells = 0;
-        let answer = "";
-        totalCells = columnCount * rowCount;
-        for(let i=0; i< totalCells; i++) {
-            answer = answer + number;
-            
-            if(number%(columnCount) == 0) {
-                answer = answer + "\n";    
-            } else {
-                answer = answer + ",";
-            }
-            number+=1;
+// show numbers from 1 to N 
+// for instance: 1 ~  9
+/*
+    1,2,3
+    4,5,6
+    7,8,9
+*/
+let showMatrix = function (columnCount, rowCount) {
+    let number = 1;
+    let totalCells = 0;
+    let answer = "";
+    totalCells = columnCount * rowCount;
+    for (let i = 0; i < totalCells; i++) {
+        answer = answer + number;
+
+        if (number % (columnCount) == 0) {
+            answer = answer + "\n";
+        } else {
+            answer = answer + ",";
         }
-        console.log(answer);
-    };
-    
-    showMatrix(3,3);
+        number += 1;
+    }
+    console.log(answer);
+};
+
+showMatrix(3, 3);
