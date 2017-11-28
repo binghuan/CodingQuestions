7f8fac327e2e5a1cb7558209f79b65060699f021
binghuan <binghuanlin@gmail.com>
2017-11-28 16:22:18 +0800

    add coding quest to show fibonacciNumbers.

---
+++ fibonacciNumbers.js
@@ -0,0 +1,30 @@
+let fibonacciNumbers = function(numberLength) {
+
+    let number = 1;
+
+    let numArray = [];
+
+    // 1st, put 1 number into array.
+    numArray.push(number);
+
+    // main function. 
+    for(let i =0; i< numberLength; i++) {
+
+        let lastOne = numArray.pop();
+        let lastTwo = numArray.pop();
+        let sum = 0;
+
+        if(lastTwo != null) {
+            sum = lastOne + lastTwo;
+            numArray.push(lastTwo);
+        } else {
+            sum = lastOne;
+        }
+        number = sum;
+        numArray.push(lastOne);
+        numArray.push(number);
+        console.log(numArray);
+    }
+}
+
+fibonacciNumbers(9);
