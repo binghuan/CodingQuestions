8435e2cbdc2222b0ba4110083872da4ff1875e83
binghuan <binghuanlin@gmail.com>
2017-12-30 02:43:08 +0800

    Add new leetcode question - no 442

---
+++ leetcode/no0442_Minimum_Moves_to_Equal_Array_Elements.js
@@ -0,0 +1,40 @@
+/*
+442. Find All Duplicates in an Array
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function(nums) {
+    
+    let duplicateNumArray = [];
+    
+    let numberTable = {};
+    for(let i =0; i< nums.length; i++) {
+        let num = nums[i];
+        if(numberTable[num] != null) {
+            if(numberTable[num] == 1) {
+                duplicateNumArray.push(num);
+            }
+            numberTable[num] += 1;
+        } else {
+            numberTable[num] = 1;
+        }
+    }
+    console.log(duplicateNumArray);
+    return duplicateNumArray;
+};
