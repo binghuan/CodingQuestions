ef1cc0345da39dc6b00d1bc0b68e8267cdcf337e
BH_Lin <binghuanlin@gmail.com>
2019-03-28 23:33:35 +0800

    add new coding question

---
+++ leetcode/no0189_Rotate_Array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  console.log('## INPUT: ', nums, 'steps=', k);
+  for (let j = 0; j < k; j++) {
+    let temp = nums[nums.length - 1] for (let i = nums.length - 1; i > 0; i--) {
+      // console.log("i=", i);
+      nums[i] = nums[i - 1];
+    }
+    nums[0] = temp;
+    console.log(' -------> ', nums, ', ROUND ', j);
+  }
+};
+
+let input = [1, 2, 3, 4, 5, 6, 7], k = 3;
+// input = [-1,-100,3,99], k = 2;
+rotate(input, k);
