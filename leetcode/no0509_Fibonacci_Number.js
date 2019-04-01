53b9fe0a353fcef09ed5009037d740451a955cb7
binghuan <binghuanlin@gmail.com>
2019-04-01 16:48:51 +0800

    Add new coding question

---
+++ leetcode/no0509_Fibonacci_Number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function(N) {
+    
+    if(N == 0) {
+        return 0;
+    }
+    
+    else if(N == 1) {
+        return 1;
+    }
+    
+    return fib(N-1) + fib(N-2)
+};
