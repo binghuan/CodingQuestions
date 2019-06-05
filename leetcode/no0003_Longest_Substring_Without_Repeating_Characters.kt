c1e5a9ed8370d3b22ae75214ed31aab2ba122223
BH_Lin <binghuanlin@gmail.com>
2019-06-06 01:06:25 +0800

    add new solution for coding question by using different programming language.

---
+++ leetcode/no0003_Longest_Substring_Without_Repeating_Characters.kt
@@ -0,0 +1,50 @@
+import java.util.Arrays
+
+
+fun main(args: Array<String>) {
+    println(">> main");
+
+    val solution = Solution();
+
+    val s = "abcabcbb";
+
+    solution.lengthOfLongestSubstring(s);
+
+}
+
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+
+        println("## INPUT: $s");
+
+        var temp = "";
+        var ans = "";
+        for (index in 0..s.length - 1) {
+
+            val char = s.get(index);
+
+            val pos = temp.indexOf(char);
+
+            println("check $char in $temp --> $pos");
+
+            temp = temp.plus(char);
+
+            if (pos == -1) {
+                println("temp => $temp");
+            } else {
+                print("replace temp from $temp");
+                temp = temp.substring(pos + 1);
+                println(" to $temp");
+            }
+
+            if (temp.length > ans.length) {
+                ans = temp;
+                println("current maxLength of substring = $ans");
+            }
+        }
+
+        println("## OUTPUT: $ans")
+
+        return ans.length;
+    }
+}
