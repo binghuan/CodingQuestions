3f5ad604ded7d66dc17c2b9ffdfbb01ce8919bbe
BH_Lin <binghuanlin@gmail.com>
2019-06-05 23:36:04 +0800

    add new coding questions

---
+++ leetcode/no0001_Two_Sum.kt
@@ -0,0 +1,38 @@
+import java.util.Arrays
+
+
+fun main(args: Array<String>) {
+    println(">> main");
+
+    val solution = Solution();
+
+    val integers = intArrayOf(2, 7, 11, 15)
+    val target = 9;
+
+    solution.twoSum(integers, target);
+
+}
+
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+
+        for (i in nums.indices) {
+            for (j in nums.indices) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (nums[i] + nums[j] == target) {
+                    println("hit ~~");
+                    val integers = intArrayOf(i, j);
+                    println("OUTPUT: ${Arrays.toString(integers)}");
+                    return integers;
+                }
+
+            }
+        }
+
+        println("OUTPUT: []");
+        return IntArray(0);
+    }
+}
