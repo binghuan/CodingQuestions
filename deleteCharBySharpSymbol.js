03262c93dad8bc1774a6e9a29d3044df28a97efd
BH_Lin <binghuanlin@gmail.com>
2021-04-17 00:39:49 +0800

    add new coding question

---
+++ deleteCharBySharpSymbol.js
@@ -0,0 +1,31 @@
+/*
+'#' is delete
+Input: S = "ab#c", T= "ab#c"
+Output: true
+Explanation: Both S and T becom "ac", 
+
+Coding Question from Grab Taipei.
+*/
+
+let strTransfer = (input) => {
+    const DBG = true;
+    console.log("INPUT :", input);
+    let result = [];
+    let chars = input.split("");
+    for (let i = 0; i < input.length; i++) {
+        let char = chars[i];
+        if (char == "#") {
+            result.pop();
+        } else {
+            result.push(char);
+        }
+    }
+
+    let ans = result.toString().replace(/,/, "");
+    console.log("OUTPUT:", ans);
+    console.log("------------");
+}
+
+strTransfer("ab#c")
+strTransfer("ab###c")
+strTransfer("#a#c")
