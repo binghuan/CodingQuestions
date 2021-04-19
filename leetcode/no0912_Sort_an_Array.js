901510002a978a74a49d9c6f29288db562ad2d01
BH_Lin <binghuanlin@gmail.com>
2021-04-19 17:32:22 +0800

    add log

--- leetcode/no0912_Sort_an_Array.js
+++ leetcode/no0912_Sort_an_Array.js
@@ -27,28 +27,33 @@ function bubbleSort(nums) {
 }
 
 function quickSort(arr, head, tail) {
+    const DBG = true;
 
-    //console.log(">> quickSort:", arr, head, tail);
+    if (DBG) console.log(">> quickSort:", arr, "head: ", head, "tail: ", tail);
 
     if (head >= tail || arr == null || arr.length <= 1) {
         return;
     }
     let pivotIndex = parseInt((head + tail) / 2);
     let i = head, j = tail, pivot = arr[pivotIndex];
-    //console.log("--> head:", head, "tail:", tail, "pivotIndex:", pivotIndex, "pivot:", pivot);
+    if (DBG) console.log("--> head:", head, "tail:", tail, "pivotIndex:", pivotIndex, "pivot:", pivot);
     while (i <= j) {
         while (arr[i] < pivot) {
+            let originali = i;
             ++i;
+            console.log(`Move head from ${originali} to ${i}`);
         }
-        //console.log("arr[", i, "]=", arr[i], " >= ", pivot, " by pivotIndex:", pivotIndex);
+        if (DBG) console.log("arr[i:", i, "]=", arr[i], " >= ", pivot, " by pivotIndex:", pivotIndex);
 
         while (arr[j] > pivot) {
+            let originalJ = j;
             --j;
+            console.log(`Move tail from ${originalJ} to ${j}`);
         }
-        //console.log("arr[", j, "]=", arr[j], " <= ", pivot, " by pivotIndex:", pivotIndex);
+        if (DBG) console.log("arr[j:", j, "]=", arr[j], " <= ", pivot, " by pivotIndex:", pivotIndex);
 
         if (i < j) {
-            //console.log("!! Swap arr[", i, "]=", arr[i], "arr[", j, "]=", arr[j]);
+            if (DBG) console.log("!! Swap arr[", i, "]=", arr[i], "arr[", j, "]=", arr[j]);
             let t = arr[i];
             arr[i] = arr[j];
             arr[j] = t;
@@ -58,8 +63,10 @@ function quickSort(arr, head, tail) {
             ++i;
         }
     }
-    //console.log("check left...");
+    if (DBG) console.log("check left...");
     quickSort(arr, head, j);
-    //console.log("check right...");
+    if (DBG) console.log("check right...");
     quickSort(arr, i, tail);
 }
+
+sortArray([5, 2, 3, 1]);
