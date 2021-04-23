9f88d9c89df3d809493ee51fa0a91e64504c46a0
BH_Lin <binghuanlin@gmail.com>
2021-04-23 16:58:14 +0800

    add new coding quest

---
+++ no1816_Truncate_Sentence.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function (s, k) {
+
+
+    let items = s.split(" ");
+    let result = "";
+    for (let i = 0; i < k; i++) {
+        result += items[i] + " ";
+    }
+
+    console.log(`result __$result__`);
+
+    return result.trim();
+};
