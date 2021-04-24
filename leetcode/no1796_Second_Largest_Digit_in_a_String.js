628f37873d03736aad72d03721ea8eb87af334b7
BH_Lin <binghuanlin@gmail.com>
2021-04-25 00:01:43 +0800

    add new coding question

---
+++ leetcode/no1796_Second_Largest_Digit_in_a_String.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var secondHighest = function (s) {
+
+    // reference: https://stackoverflow.com/questions/1862130/strip-all-non-numeric-characters-from-string-in-javascript
+    // Use the string's .replace method with a regex of \D, which is a shorthand character class that matches all non-digits
+    let numberOnlyString = s.replace(/\D/g, '').split("");
+    let sortedArray = numberOnlyString.sort((a, b) => { return b - a });
+    if (sortedArray.length == 0) {
+        return -1;
+    }
+    console.log(sortedArray[1]);
+    function onlyUnique(value, index, self) {
+        return self.indexOf(value) === index;
+    }
+    var unique = sortedArray.filter(onlyUnique);
+    if (unique.length == 1) {
+        return -1
+    }
+
+
+    return unique[1]
+};
