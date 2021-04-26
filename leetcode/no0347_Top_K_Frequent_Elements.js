50e72e58b0f5815850c7a4b0d7af307e66514e38
BH_Lin <binghuanlin@gmail.com>
2021-04-27 01:04:49 +0800

    Create no0347_Top_K_Frequent_Elements.js

---
+++ leetcode/no0347_Top_K_Frequent_Elements.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+    const DBG = true;
+    let map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let number = nums[i];
+        if (map[number] == null) {
+            map[number] = 1;
+        } else {
+            map[number] += 1;
+        }
+    }
+
+    let countedArray = [];
+    for (let i = 0; i < Object.keys(map).length; i++) {
+        let number = Object.keys(map)[i];
+        countedArray.push(
+            {
+                value: number,
+                count: map[number]
+            });
+    }
+
+    let result = [];
+    let sortedArray = countedArray.sort((a, b) => { return b.value - a.value });
+    if (DBG) console.log("sortedArray:", sortedArray)
+    for (let i = 0; i < k; i++) {
+        result.push(sortedArray[i].count);
+    }
+
+    return result;
+};
+
+
+topKFrequent([1, 1, 1, 2, 2, 3])
