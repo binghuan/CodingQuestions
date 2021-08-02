ed96984af8cebd381f143c2d3ef7fc6f67b74e91
BH_Lin <binghuanlin@gmail.com>
2021-08-03 01:37:44 +0800

    add new question

---
+++ leetcode/no1929_Concatenation_of_Array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+
+    let result = [];
+    result = nums.concat(nums);
+
+    return result;
+
+};
