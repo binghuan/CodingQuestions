e5281607f656b9433c34d01a8bdbb668a560a122
BH_Lin <binghuanlin@gmail.com>
2021-09-06 18:02:03 +0800

    Create no0078_Subsets.js

---
+++ leetcode/no0078_Subsets.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+
+    function backtrack(startedIndex, currentCombination, nums, answer, lengthOfNums) {
+
+        if (currentCombination.length == lengthOfNums) {
+            answer.push(currentCombination.slice(0));
+            //console.log("Add", currentCombination);
+            return;
+        }
+
+        for (let i = startedIndex; i < nums.length; i++) {
+            currentCombination.push(nums[i]);
+
+            backtrack(
+                i + 1,
+                currentCombination,
+                nums,
+                output,
+                lengthOfNums
+            )
+            currentCombination.pop();
+        }
+    }
+
+    let curr = [];
+    let output = [];
+
+    for (let i = 0; i <= nums.length; i++) {
+
+        backtrack(
+            0,
+            curr,
+            nums,
+            output,
+            i
+        )
+    }
+
+    return output;
+};
