9e61955a3078d7ac4994918c0151a067150c96d0
BH_Lin <binghuanlin@gmail.com>
2021-09-12 00:32:29 +0800

    Create no0199_Binary_Tree_Right_Side_View.js

---
+++ leetcode/no0199_Binary_Tree_Right_Side_View.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+
+    let set = new Set();
+    let result = [];// number[]
+
+    function traverse(node, depth) {
+
+        if (node == null) {
+            return;
+        }
+
+        if (!set.has(depth)) {
+            set.add(depth);
+            result.push(node.val);
+        }
+
+        if (node.right != null) {
+            traverse(node.right, depth + 1);
+        }
+
+        if (node.left != null) {
+            traverse(node.left, depth + 1);
+        }
+    }
+
+    traverse(root, 0);
+    return result;
+};
