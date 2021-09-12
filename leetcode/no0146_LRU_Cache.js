7efc8500aaf606f39a9a1d9ae86e063a233f19ce
BH_Lin <binghuanlin@gmail.com>
2021-09-12 18:58:37 +0800

    Create no0146_LRU_Cache.js

---
+++ leetcode/no0146_LRU_Cache.js
@@ -0,0 +1,105 @@
+/**
+ * @param {number} capacity
+ */
+
+let map = new Map();
+let valueArray = [];
+let maxCap = 0;
+let actionNo = 0;
+const DBG = false;
+
+var LRUCache = function (capacity) {
+    actionNo = 0;
+    myMap = new Map();
+    map.clear()
+    maxCap = capacity;
+    valueArray = [];
+    if (DBG) console.log("#", actionNo, "Init with capacity", capacity);
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+
+    actionNo += 1;
+    let result = map.get(key);
+    if (result == null) {
+        result = -1;
+    } else {
+        cleanUpValueArray(key);
+    }
+    if (DBG) console.log("#", actionNo, "get", key, "-> ", result);
+    if (DBG) console.log(map);
+    if (DBG) console.log("values:", valueArray);
+
+    return result;
+
+};
+
+function binarySearch(arr, val) {
+    var low = 0,
+        high = arr.length - 1;
+    while (low <= high) {
+        var mid = parseInt((low + high) / 2);
+        if (val == arr[mid]) {
+            return mid;
+        } else if (val > arr[mid]) {
+            low = mid + 1;
+        } else if (val < arr[mid]) {
+            high = mid - 1;
+        }
+    }
+    return -1;
+};
+
+function cleanUpValueArray(key) {
+    if (DBG) console.log(">> cleanValueArray", key);
+
+    let targetIndex = binarySearch(valueArray.slice(0).sort((a, b) => {
+        return a - b;
+    }), key);
+
+    if (targetIndex != -1) {
+        for (let i = 0; i < valueArray.length; i++) {
+            let theKey = valueArray[i];
+            if (theKey == key) {
+                valueArray.splice(i, 1);
+                valueArray.push(theKey);
+                break;
+            }
+        }
+    } else {
+        valueArray.push(key);
+    }
+
+    if (valueArray.length > maxCap) {
+        let removedKey = valueArray.shift();
+        map.delete(removedKey);
+    }
+    if (DBG) console.log("Values:", valueArray);
+}
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+    actionNo += 1;
+    if (DBG) console.log("#", actionNo, "put", key, value);
+
+    cleanUpValueArray(key);
+    map.set(key, value)
+
+    if (DBG) console.log("-->", valueArray);
+    if (DBG) console.log(map);
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
