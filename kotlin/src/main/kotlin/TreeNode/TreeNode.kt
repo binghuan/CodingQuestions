3810b46f262c3695c0349928f6715a0c94b9e836
BH_Lin <binghuanlin@gmail.com>
2021-09-16 18:38:29 +0800

    add files for kotlin

---
+++ kotlin/src/main/kotlin/TreeNode/TreeNode.kt
@@ -0,0 +1,5 @@
+package TreeNode
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
---
+++ kotlin/src/main/kotlin/no0199_Binary_Tree_Right_Side_View.kt
@@ -0,0 +1,48 @@
+import TreeNode.TreeNode
+
+fun main(args: Array<String>) {
+
+
+    /**
+     * Example:
+     * var ti = TreeNode(5)
+     * var v = ti.`val`
+     * Definition for a binary tree node.
+     * class TreeNode(var `val`: Int) {
+     *     var left: TreeNode? = null
+     *     var right: TreeNode? = null
+     * }
+     */
+    class Solution {
+        fun rightSideView(root: TreeNode?): List<Int> {
+
+            val result = arrayListOf<Int>()
+            var mySet = setOf<Int>()
+
+            fun traverse(node: TreeNode, depth: Int) {
+
+                if (node == null) {
+                    return
+                }
+
+                if (!mySet.contains(depth)) {
+                    mySet += depth
+                    result += node.`val`
+                }
+
+                if (node.right != null) {
+                    traverse(node.right!!, depth + 1)
+                }
+
+                if (node.left != null) {
+                    traverse(node.left!!, depth + 1)
+                }
+            }
+            root?.let { traverse(it, 0) }
+
+            return result
+        }
+    }
+
+    println(">> main");
+}
