0c196a71aa67019143533a968ee11417181a5f11
BH_Lin <binghuanlin@gmail.com>
2021-09-16 23:40:06 +0800

    Create no0144_Binary_Tree_Preorder_Traversal.js

---
+++ leetcode/no0144_Binary_Tree_Preorder_Traversal.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+    let answer = [];
+    function traverse(node) {
+        if (node == null) {
+            return;
+        }
+        answer.push(node.val);
+        if (node.left != null) {
+            traverse(node.left);
+        }
+        if (node.right != null) {
+            traverse(node.right);
+        }
+    }
+    traverse(root);
+    return answer;
+};
+
+// reference: https://ithelp.ithome.com.tw/articles/10205571
