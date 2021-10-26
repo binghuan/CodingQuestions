1737be72fe651231b22b10160c6ddbf965c436be
BH_Lin <binghuanlin@gmail.com>
2021-10-26 16:35:36 +0800

    Update no0905_Sort_Array_By_Parity.js

--- leetcode/no0905_Sort_Array_By_Parity.js
+++ leetcode/no0905_Sort_Array_By_Parity.js
@@ -4,54 +4,9 @@
  */
 var sortArrayByParity = function (A) {
 
-    let numsArray = [];
-    if (A.length == 1) {
-        return A;
-    } else if (A.length == 2) {
-        return A.sort((a, b) => {
-            return a - b
-        });
-    }
+    A.sort((a, b) => {
+        return a % 2 - b % 2
+    })
 
-    let result = [];
-    for (let i = 0; i < A.length; i++) {
-        let num1 = A[i]
-        let num2 = 0;
-        let numbers = [num1];
-        if (A.length  > (i + 1)) {
-            num2 = A[i + 1]
-            numbers.push(num2);
-        }
-        console.log("check ", num1, num2);
-
-        numsArray.push(
-            {
-                total: num1 + num2,
-                numbers: numbers
-            }
-        );
-        i = i + 1;
-    };
-
-    numsArray.sort((a, b) => {
-        return b.total - a.total
-    });
-    console.log(numsArray);
-    for (let i = 0; i < numsArray.length; i++) {
-        if(numsArray[i].numbers.length > 1) {
-            result.push(numsArray[i].numbers[0]);
-            result.push(numsArray[i].numbers[1]);
-            console.log("push 2:", numsArray[i]);
-        } else {
-            result.push(numsArray[i].numbers[0]);
-            console.log("push 1:", numsArray[i]);
-        }
-        
-    }
-    console.log(result);
-    return result;
+    return A;
 };
-
-//sortArrayByParity([3, 1, 2, 4]);
-//sortArrayByParity([0, 2, 4]);
-sortArrayByParity([0, 1, 3]);
