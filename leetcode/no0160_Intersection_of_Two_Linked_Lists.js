d680c094da63cf31c5016a0f0a31e3010981ada5
BH_Lin <binghuanlin@gmail.com>
2021-11-01 14:04:56 +0800

    Create no0160_Intersection_of_Two_Linked_Lists.js

---
+++ leetcode/no0160_Intersection_of_Two_Linked_Lists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+
+    let index = 0;
+
+    while (headA != null) {
+        headA.tag = index;
+        index += 1;
+        headA = headA.next;
+    }
+
+    while (headB != null) {
+
+        if (headB.tag != null) {
+            return headB;
+        }
+        headB = headB.next;
+    }
+
+    return null;
+};
