19a28711ae05cce1f88fe0c138d03e73fb9ad8b6
BH_Lin <binghuanlin@gmail.com>
2021-11-05 11:56:23 +0800

    Create no0195_Tenth_Line.sh

---
+++ leetcode/no0195_Tenth_Line.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+index=1
+lines=$(cat file.txt)
+while read line ; do 
+
+    echo "#${index}: ${line}"
+    if [ $index == 10 ]; then 
+        echo $line
+    fi
+
+    (( index = index + 1 ))
+
+done <<< "echo $lines"
