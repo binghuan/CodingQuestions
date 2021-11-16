45eb7595fa3b996412f1c1b58224bae8c1ee3a31
BH_Lin <binghuanlin@gmail.com>
2021-11-16 17:25:07 +0800

    Update quickSort.js

--- quickSort.js
+++ quickSort.js
@@ -1,40 +1,53 @@
-function quickSort(arr, head, tail) {
-
-    console.log(">> quickSort:", arr, head, tail);
-
-    if (head >= tail || arr == null || arr.length <= 1) {
-        return;
-    }
-    let pivotIndex = parseInt((head + tail) / 2);
-    let i = head, j = tail, pivot = arr[pivotIndex];
-    console.log("--> head:", head, "tail:", tail, "pivotIndex:", pivotIndex, "pivot:", pivot);
-    while (i <= j) {
-        while (arr[i] < pivot) {
-            ++i;
-        }
-        console.log("arr[", i, "]=", arr[i], " >= ", pivot, " by pivotIndex:", pivotIndex);
-
-        while (arr[j] > pivot) {
-            --j;
-        }
-        console.log("arr[", j, "]=", arr[j], " <= ", pivot, " by pivotIndex:", pivotIndex);
-
-        if (i < j) {
-            console.log("!! Swap arr[", i, "]=", arr[i], "arr[", j, "]=", arr[j]);
-            let t = arr[i];
-            arr[i] = arr[j];
-            arr[j] = t;
-            ++i;
-            --j;
-        } else if (i == j) {
-            ++i;
-        }
-    }
-    console.log("check left...");
-    quickSort(arr, head, j);
-    console.log("check right...");
-    quickSort(arr, i, tail);
+function swap(array, left, right) {
+    let temp = array[left];
+    array[left] = array[right];
+    array[right] = temp;
 }
 
-let input = [1, 4, 8, 2, 55, 3, 4, 8, 6, 4, 0, 11, 34, 90, 23, 54, 77, 9, 2, 9, 4, 10];
+function partition(array, left, right, pivot) {
+    console.log("+++ partition +++", array, "left=", left, "right=", right, "pivot=", pivot);
+    while (left <= right) {
+        while (array[left] < pivot) {
+            left++;
+        }
+        while (array[right] > pivot) {
+            right--;
+        }
+        if (left <= right) {
+            swap(array, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    console.log("--- partition ---", array, "left=", left, "right=", right, "pivot=", pivot);
+    return left;
+}
+
+let numberOfCall = 0;
+function quickSort(array, left, right) {
+
+    let indexOfCall = ++numberOfCall;
+    console.log("#", indexOfCall++, "+++ quickSort +++", array, "left=", left, "right=", right);
+
+    if (left >= right) {
+        return;
+    }
+
+    let pivot = array[parseInt((left + right) / 2)];
+
+    let index = partition(array, left, right, pivot);
+
+    quickSort(array, left, index - 1);
+    quickSort(array, index, right);
+
+    console.log("#", indexOfCall, "--- quickSort ---", array, "left=", left, "right=", right);
+}
+
+let input = [5, 3, 1, 2, 4];
+console.log("INPUT  :", input);
 quickSort(input, 0, input.length - 1);
+console.log("OUOTPUT:", input);
+
+
+// Reference: https://www.youtube.com/watch?app=desktop&v=SLauY6PpjW4&ab_channel=HackerRank
