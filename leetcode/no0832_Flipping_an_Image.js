432fc2f473069e66492819e63932f4f65b2f60bd
BH_Lin <binghuanlin@gmail.com>
2021-11-17 15:08:54 +0800

    Create no0832_Flipping_an_Image.js

---
+++ leetcode/no0832_Flipping_an_Image.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function (image) {
+
+    for (let i = 0; i < image.length; i++) {
+        
+        image[i] = image[i].reverse();
+
+        for (let j = 0; j < image[i].length; j++) {
+
+            if (image[i][j] == 0) {
+                image[i][j] = 1;
+            } else {
+                image[i][j] = 0;
+            }
+        }
+    }
+
+    return image;
+};
