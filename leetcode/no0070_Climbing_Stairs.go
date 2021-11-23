dd85a3659a1fbb209bc9a89e6a83f5940dce3c32
BH_Lin <binghuanlin@gmail.com>
2021-11-23 14:46:09 +0800

    Create no0070_Climbing_Stairs.go

---
+++ leetcode/no0070_Climbing_Stairs.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func climbStairs(n int) int {
+
+	stepHistory := map[int]int{}
+
+	var doClimb func(steps int) int
+
+	doClimb = func(steps int) int {
+
+		if steps == 1 {
+			return 1
+		}
+
+		if steps == 2 {
+			return 2
+		}
+
+		if steps <= 0 {
+			return 0
+		}
+
+		_, isExisted := stepHistory[steps]
+		if isExisted {
+			return stepHistory[steps]
+		}
+
+		total := doClimb(steps-1) + doClimb(steps-2)
+		stepHistory[n] = total
+		return total
+	}
+
+	return doClimb(n)
+}
+
+func main() {
+
+	input := 2
+	fmt.Println("INPUT:", input)
+	result := climbStairs(input)
+	fmt.Println("OUTPUT:", result)
+}
