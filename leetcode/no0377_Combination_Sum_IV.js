27b6f80947be6521f876258f925347b572fc33a8
BH_Lin <binghuanlin@gmail.com>
2021-11-25 17:33:48 +0800

    Update no0377_Combination_Sum_IV.js

--- leetcode/no0377_Combination_Sum_IV.js
+++ leetcode/no0377_Combination_Sum_IV.js
@@ -6,32 +6,37 @@
 var combinationSum4 = function (nums, target) {
 
     console.log("INPUT:", nums, target);
-    let dp = []; // dp[i] # of combinations sum up to i
-    dp[0] = 1;
+    let pathsToTargetSum = []; // pathsToTargetSum[i] # of combinations sum up to i
+    pathsToTargetSum[0] = 1;
 
-    console.log("Before Start:", dp);
+    for (let i = 1; i <= target; i++) {
+        pathsToTargetSum[i] = 0;
+    }
+    console.log("init table=", pathsToTargetSum);
 
     for (let value = 1; value <= target; ++value) {
-
-        dp[value] ? null : dp[value] = 0;
 
         console.log("check value:", value);
 
         nums.forEach((choosedNum) => {
-
             console.log("Take", choosedNum, "from", nums);
             let remaining = value - choosedNum;
             console.log(`value: ${value} - num: ${choosedNum} = ${remaining}`);
             if (remaining >= 0) {
-                dp[value] += dp[remaining];
+                pathsToTargetSum[value] += pathsToTargetSum[remaining];
             }
-            console.log(dp)
+            console.log(pathsToTargetSum)
         })
     }
-    let result = dp[target];
-    console.log(dp);
+    let result = pathsToTargetSum[target];
+    console.log(pathsToTargetSum);
     console.log("OUTPUT:", result);
     return result;
 };
 
-combinationSum4([1, 2, 3], 4)
+//combinationSum4([1, 2, 3], 4)
+combinationSum4([1, 2], 2)
+/*
+(1,1)
+(2)
+*/
