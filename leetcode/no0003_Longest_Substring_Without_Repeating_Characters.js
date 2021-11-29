643ae340e02e5080676d0b98cd3376f850f93b9f
BH_Lin <binghuanlin@gmail.com>
2021-11-29 14:33:22 +0800

    Update no0003_Longest_Substring_Without_Repeating_Characters.js

--- leetcode/no0003_Longest_Substring_Without_Repeating_Characters.js
+++ leetcode/no0003_Longest_Substring_Without_Repeating_Characters.js
@@ -16,21 +16,31 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
+ var lengthOfLongestSubstring = function (s) {
+    console.log("INPUT:", s);
     let length = s.length;
     let charIndexMap = {};
     let startIndex = 0;
     let maxLength = 0;
     for (let i = 0; i < length; i++) {
         let char = s[i];
-        if(charIndexMap[char] == null) {
+        if (DBG) console.log(`+++ Start checking char by index[${i}]= ${char} +++`);
+        if (charIndexMap[char] == null) {
             charIndexMap[char] = -1;
+            console.log(`"${char}" was not found in charIndexMap`, charIndexMap);
+        } else {
+            console.log(`"${char}" was found in charIndexMap`, charIndexMap);
         }
-        startIndex = Math.max(startIndex,charIndexMap[s[i]] + 1);
-        maxLength = Math.max(maxLength, i - startIndex + 1); 
+        if (DBG) console.log(`startIndex = ${startIndex}, charIndexMap[s[i]] = ${charIndexMap[s[i]]}`);
+        let bypassIndexOfChar = charIndexMap[s[i]] + 1;
+        startIndex = Math.max(startIndex, bypassIndexOfChar);
+        if (DBG) console.log("startIndex -> ", startIndex);
+        let currentLength = i - startIndex + 1;
+        if (DBG) console.log(`last max length of non-repeating string = ${maxLength}, now is ${currentLength}`);
+        maxLength = Math.max(maxLength, currentLength);
+        if (DBG) console.log("!!max length = ", maxLength);
         charIndexMap[char] = i;
     }
-
     console.log("OUTPUT:", maxLength);
     return maxLength;
 };
