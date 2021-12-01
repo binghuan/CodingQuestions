ecfac90a675e5221741d902a1a88723d10af508d
BH_Lin <binghuanlin@gmail.com>
2021-12-01 14:54:34 +0800

    Update no0179_Largest_Number.js

--- leetcode/no0179_Largest_Number.js
+++ leetcode/no0179_Largest_Number.js
@@ -5,7 +5,7 @@
 var largestNumber = function (nums) {
 
     nums.sort((a, b) => {
-        return `${a}${b}` < `${b}${a}`;
+        return `${b}${a}` - `${a}${b}`;
     });
 
     console.log("sortedNums:", nums);
@@ -17,4 +17,17 @@ var largestNumber = function (nums) {
 
 // Input: nums = [3,30,34,5,9]
 // Output: "9534330"
-largestNumber([3,30,34,5,9]);
+largestNumber([3, 30, 34, 5, 9]);
+//largestNumber([1, 3, 2]);
+
+//let input = [10,2];
+//let input = [3,30,34,5,9];
+// let input = [9, 9009, 909];
+// console.log(input.sort((a,b) => {
+//     //return b-a;
+//     let _a = `${b}${a}`;
+//     let _b = `${a}${b}`;
+//     result = _a - _b;
+//     console.log("compare", _a, _b, result);
+//     return result;
+// }));
