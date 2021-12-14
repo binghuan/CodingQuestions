2403b226de1ca9046273ec9116018582c7a31d3a
BH_Lin <binghuanlin@gmail.com>
2021-12-14 17:19:58 +0800

    Create no2089_Find_Target_Indices_After_Sorting_Array.js

---
+++ leetcode/no2089_Find_Target_Indices_After_Sorting_Array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function (nums, target) {
+
+    let result = [];
+    nums.sort((a, b) => {
+        return a - b;
+    })
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == target) {
+            result.push(i);
+        }
+    }
+
+    return result;
+};
