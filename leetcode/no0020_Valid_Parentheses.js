7e75b742570f8c4edd675b8c1744f78705c93c4c
BH_Lin <binghuanlin@gmail.com>
2021-12-14 14:49:51 +0800

    Update no0020_Valid_Parentheses.js

--- leetcode/no0020_Valid_Parentheses.js
+++ leetcode/no0020_Valid_Parentheses.js
@@ -4,46 +4,32 @@
  * @return {boolean}
  */
  var isValid = function (s) {
-
-
-    let checkedChars = [];
     let result = true;
-
+    let checkedChars = [];
     for (let i = 0; i < s.length; i++) {
         let char = s[i];
 
-        if (char == "(" ||
-            char == "{" ||
-            char == "[") {
-
+        if (char.match(/\(|\{|\[/) != null) {
             checkedChars.push(char);
-
-        } else if (char == ")" ||
-            char == "}" ||
-            char == "]") {
-
-
+        } else if (char.match(/\)|\}|\]/) != null) {
             let symbol = checkedChars.pop();
-            let isNeededToRollback = false;
+            let isBraceketMatched = true;
             if (char == ")" && symbol != "(") {
-                isNeededToRollback = true;
+                isBraceketMatched = false;
             } else if (char == "}" && symbol != "{") {
-                isNeededToRollback = true;
+                isBraceketMatched = false;
             } else if (char == "]" && symbol != "[") {
-                isNeededToRollback = true;
+                isBraceketMatched = false;
             }
 
-            if (isNeededToRollback) {
-                checkedChars.push(symbol);
+            if (!isBraceketMatched) {
                 result = false;
+                break;
             }
         }
     }
-
     if (checkedChars.length > 0) {
         result = false;
     }
-
     return result;
-
 };
