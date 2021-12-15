7cbecfc2db585d38fc096d87c52d9bdaecf47605
BH_Lin <binghuanlin@gmail.com>
2021-12-15 17:58:35 +0800

    Update no0273_Integer_to_English_Words.js

--- leetcode/no0273_Integer_to_English_Words.js
+++ leetcode/no0273_Integer_to_English_Words.js
@@ -6,60 +6,26 @@ var numberToWords = function (num) {
 
     const DBG = false;
 
-    let unitTable = [
-        {
-            unit: "Trillion",
-            value: 1000000000000
-        },
-        {
-            unit: "Billion",
-            value: 1000000000
-        },
-        {
-            unit: "Million",
-            value: 1000000
-        },
-        {
-            unit: "Thousand",
-            value: 1000
-        }
-    ]
-
-    let numberToSpeech = {
-        1: "One",
-        2: "Two",
-        3: "Three",
-        4: "Four",
-        5: "Five",
-        6: "Six",
-        7: "Seven",
-        8: "Eight",
-        9: "Nine",
-        10: "Ten",
-        11: "Eleven",
-        12: "Twelve",
-        13: "Thirteen",
-        14: "Fourteen",
-        15: "Fifteen",
-        16: "Sixteen",
-        17: "Seventeen",
-        18: "Eighteen",
-        19: "Nineteen",
-        20: "Twenty",
-        30: "Thirty",
-        40: "Forty",
-        50: "Fifty",
-        60: "Sixty",
-        70: "Seventy",
-        80: "Eighty",
-        90: "Ninety",
-        100: "Hundred",
-    }
-
     if (num == 0) {
         return "Zero";
     } else if (num == 100) {
         return "One Hundred";
+    }
+
+    let unitTable = [
+        { unit: "Trillion", value: 1000000000000 },
+        { unit: "Billion", value: 1000000000 },
+        { unit: "Million", value: 1000000 },
+        { unit: "Thousand", value: 1000 }
+    ]
+
+    let numberToSpeech = {
+        1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five",
+        6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", 10: "Ten",
+        11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen",
+        16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty",
+        30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", 70: "Seventy",
+        80: "Eighty", 90: "Ninety", 100: "Hundred"
     }
 
     for (let i = 21; i <= 999; i++) {
@@ -98,10 +64,8 @@ var numberToWords = function (num) {
     for (let i = 0; i < unitTable.length; i++) {
         if (DBG) console.log("check num:", num);
         while (num / unitTable[i].value >= 1) {
-
             // numbersOfUnit = 1 ~ 999
             let numbersOfUnit = parseInt(num / unitTable[i].value);
-
             num -= unitTable[i].value * numbersOfUnit;
             let space = "";
             if (result.length > 0) {
