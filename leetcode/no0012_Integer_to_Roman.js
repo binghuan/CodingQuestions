7eca14c24ccc2d711dc9559a83984d39d0053197
BH_Lin <binghuanlin@gmail.com>
2021-12-15 15:02:36 +0800

    Update no0012_Integer_to_Roman.js

--- leetcode/no0012_Integer_to_Roman.js
+++ leetcode/no0012_Integer_to_Roman.js
@@ -3,20 +3,35 @@
  * @return {string}
  */
 var intToRoman = function (num) {
-    const DBG = false;
-    let amountForUnit = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    let symbolList = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+    const DBG = true;
+
+    let symbolArray = [
+        { key: "M", value: 1000 },
+        { key: "CM", value: 900 },
+        { key: "D", value: 500 },
+        { key: "CD", value: 400 },
+        { key: "C", value: 100 },
+        { key: "XC", value: 90 },
+        { key: "L", value: 50 },
+        { key: "XL", value: 40 },
+        { key: "X", value: 10 },
+        { key: "IX", value: 9 },
+        { key: "V", value: 5 },
+        { key: "IV", value: 4 },
+        { key: "I", value: 1 },
+    ];
+
     let result = [];
-    for (let i = 0; i < amountForUnit.length; i++) {
-        while (num / amountForUnit[i] >= 1) {
+    for (let i = 0; i < symbolArray.length; i++) {
+        while (num / symbolArray[i].value >= 1) {
             if (DBG) console.log("from num:", num);
-            let numbersOfUnit = parseInt(num / amountForUnit[i]);
+            let numbersOfUnit = parseInt(num / symbolArray[i].value);
 
             for (let j = 0; j < numbersOfUnit; j++) {
-                result.push(symbolList[i]);
+                result.push(symbolArray[i].key);
                 if (DBG) console.log("result =", result);
             }
-            num = num - numbersOfUnit * amountForUnit[i];
+            num -= numbersOfUnit * symbolArray[i].value;
             if (DBG) console.log("to num:", num);
         }
     }
