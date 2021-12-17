522911f3c1b08b291d2d35f47c830d3092050f69
BH_Lin <binghuanlin@gmail.com>
2021-12-17 17:48:21 +0800

    Update no0394_Decode_String.js

--- leetcode/no0394_Decode_String.js
+++ leetcode/no0394_Decode_String.js
@@ -3,61 +3,44 @@
  * @return {string}
  */
 var decodeString = function (s) {
-    const DBG = false;
+    const DBG = true;
     if (DBG) console.log("INPUT:", s);
-    let buffer = "";
-    let stack = [];
-    let level = 0;
-    let result = [[]];
+    let buffer = "", multiplierStack = [], currLevel = 0, result = [[]];
     for (let i = 0; i < s.length; i++) {
         let char = s[i];
-        
-        if (DBG) console.log("Checking:", char);
-        if (DBG) console.log("---- level = ", level);
+        if (DBG) console.log("index=", i, `---- Checking: "${char}", level = ${currLevel}`, "Buffer:", buffer, "Stack:", multiplierStack, "Result=", result);
         if (char == "[") {
-            level += 1;
-            stack.push(buffer);
-            if (DBG) console.log("Push", buffer, "to stack=", stack);
+            currLevel += 1;
+            multiplierStack.push(buffer);
+            if (DBG) console.log("* Push", buffer, "into stack=", multiplierStack);
             buffer = "";
         } else if (char == "]") {
-            level -= 1;
-            if (result[level] == null) {
-                result[level] = [];
-            }
-            if (result[level + 1] == null) {
-                result[level + 1] = [];
-            }
-
-            if (DBG) console.log("stack = ", stack);
-            let previous = stack.pop();
-            buffer = result[level + 1].join() + buffer;
-            result[level + 1] = [];
-
-            if (DBG) console.log("buffer =", buffer, "X", previous);
-
-            let times = previous.replace(/[a-z]/g, "");
-            let restOfBuffer = previous.replace(times, "");
-            if (DBG) console.log("Get Times:", times, "rest of buffer=", restOfBuffer);
+            currLevel -= 1;
+            if (result[currLevel] == null) { result[currLevel] = []; }
+            if (result[currLevel + 1] == null) { result[currLevel + 1] = []; }
+            let multiplier = multiplierStack.pop();
+            buffer = result[currLevel + 1].join() + buffer;
+            result[currLevel + 1] = [];
+            if (DBG) console.log("* buffer(", buffer, ") X (", multiplier, ")");
+            let times = multiplier.replace(/[a-z]/g, "");
+            let restOfBuffer = multiplier.replace(times, "");
+            if (DBG) console.log(`* Times: ${times}, rest of buffer= "${restOfBuffer}"`);
             let temp = "";
             for (let i = 0; i < parseInt(times); i++) {
                 temp = `${temp}${buffer}`
             }
-            if (DBG) console.log("= ", temp, `--> ${restOfBuffer}${temp}`);
+            if (DBG) console.log("* New Result ->", temp, `--> ${restOfBuffer}${temp}`);
             temp = `${restOfBuffer}${temp}`;
-
-
             buffer = "";
-            if (DBG) console.log("level=", level, "result=", result);
-            result[level].push(temp);
-            if (DBG) console.log("result ==>", result);
-            let tempToPush = result[level].join("");
-            result[level] = [tempToPush];
-
-            if (DBG) console.log("result ===>", result);
-
+            if (DBG) console.log("* level =", currLevel, ", result=", result);
+            result[currLevel].push(temp);
+            if (DBG) console.log(`* Push ${temp} to result =>`,result);
+            let tempToPush = result[currLevel].join("");
+            result[currLevel] = [tempToPush];
+            if (DBG) console.log("* Finalize Result on the level =>", result);
         } else {
             buffer = `${buffer}${char}`
-            if (DBG) console.log("... buffer ->", buffer);
+            if (DBG) console.log(`* Put "${char}" into buffer = ${buffer}`);
         }
     }
     if (buffer != "") {
@@ -68,15 +51,15 @@ var decodeString = function (s) {
     return output;
 };
 
-if (decodeString("sd2[f2[e]g]i") != "sdfeegfeegi") { console.log("NG> FAIL"); return };
-if (decodeString("leetcode") != "leetcode") { console.log("NG> FAIL"); return };
-if (decodeString("3[2[bc]1[d]]") != "bcbcdbcbcdbcbcd") { console.log("NG> FAIL"); return };
-if (decodeString("3[a]2[bc]") != "aaabcbc") { console.log("NG> FAIL"); return };
+//if (decodeString("sd2[f2[e]g]i") != "sdfeegfeegi") { console.log("NG> FAIL"); return };
+// if (decodeString("leetcode") != "leetcode") { console.log("NG> FAIL"); return };
+// if (decodeString("3[2[bc]1[d]]") != "bcbcdbcbcdbcbcd") { console.log("NG> FAIL"); return };
+// if (decodeString("3[a]2[bc]") != "aaabcbc") { console.log("NG> FAIL"); return };
 if (decodeString("3[a2[c]]") != "accaccacc") { console.log("NG> FAIL"); return };
-if (decodeString("2[abc]3[cd]ef") != "abcabccdcdcdef") { console.log("NG> FAIL"); return };
-if (decodeString("abc3[cd]xyz") != "abccdcdcdxyz") { console.log("NG> FAIL"); return };
-if (decodeString("100[leetcode]") != "leetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcode") { console.log("NG> FAIL"); return };
-if (decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef") != "zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef") { console.log("NG> FAIL"); return };
+// if (decodeString("2[abc]3[cd]ef") != "abcabccdcdcdef") { console.log("NG> FAIL"); return };
+// if (decodeString("abc3[cd]xyz") != "abccdcdcdxyz") { console.log("NG> FAIL"); return };
+// if (decodeString("100[leetcode]") != "leetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcode") { console.log("NG> FAIL"); return };
+// if (decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef") != "zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef") { console.log("NG> FAIL"); return };
 console.log("\nOK> ALL PASS!!")
 /*
     3[z]: zzz
