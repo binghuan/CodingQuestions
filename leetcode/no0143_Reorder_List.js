e42c118715fa5aa59f7221e7da937440014c612e
BH_Lin <binghuanlin@gmail.com>
2021-12-22 18:31:54 +0800

    Update no0143_Reorder_List.js

--- leetcode/no0143_Reorder_List.js
+++ leetcode/no0143_Reorder_List.js
@@ -12,23 +12,23 @@
 var reorderList = function (head) {
     let node = head;
     let index = 0;
-    let map = {};
+    let map = new Map();
     // Step 1: Create map to record every node's address
     while (node != null) {
-        map[index] = node;
+        map.set(index, node);
         node = node.next;
         index += 1;
     }
 
     // Step 2: Change Pointer for Next Node.
-    let size = Object.keys(map).length;
     let indexN = -1;
+    let size = map.size;
     for (let i = 0; i < parseInt(size / 2); i++) {
         indexN = size - 1 - i;
-        map[indexN].next = null;
-        map[i].next = null;
-        map[i].next = map[indexN];
-        map[indexN].next = map[i + 1];
-        map[i + 1].next = null;
+        map.get(indexN).next = null;
+        map.get(i).next = null;
+        map.get(i).next = map.get(indexN);
+        map.get(indexN).next = map.get(i + 1);
+        map.get(i + 1).next = null;
     }
 };
