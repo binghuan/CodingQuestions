489944d6681947ca4fcc8a009e4069f394d2b3e8
BH_Lin <binghuanlin@gmail.com>
2021-12-24 17:08:46 +0800

    Create no0509_Fibonacci_Number_v2.go

---
+++ leetcode/no0509_Fibonacci_Number_v2.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func fib(n int) int {
+	var recursion func(num int) int
+	recursion = func(num int) int {
+		if num <= 0 {
+			return 0
+		} else if num == 1 {
+			return 1
+		}
+		return recursion(num-1) + recursion(num-2)
+	}
+	return recursion(n)
+}
+
+func main() {
+	// 	Input: n = 2
+	// Output: 1
+	// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+	n := 2
+	fmt.Println("INPUT:", n)
+	result := fib(n)
+	fmt.Println("OUTPUT:", result)
+}
