093e40910118b9864a8c6358369e0c71800e6a89
BH_Lin <binghuanlin@gmail.com>
2021-12-24 17:08:43 +0800

    Create no0509_Fibonacci_Number_v1.go

---
+++ leetcode/no0509_Fibonacci_Number_v1.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func fib(n int) int {
+
+	if n <= 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+
+	return fib(n-1) + fib(n-2)
+}
+
+func main() {
+	// 	Input: n = 2
+	// Output: 1
+	// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+	n := 2
+	fmt.Println("INPUT:", n)
+	result := fib(n)
+	fmt.Println("OUTPUT:", result)
+}
