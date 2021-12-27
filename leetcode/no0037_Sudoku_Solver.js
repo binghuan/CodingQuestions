5f7d55e1405b345ab316a088e47e2f72b92cac6e
BH_Lin <binghuanlin@gmail.com>
2021-12-27 17:07:06 +0800

    Update no0037_Sudoku_Solver.js

--- leetcode/no0037_Sudoku_Solver.js
+++ leetcode/no0037_Sudoku_Solver.js
@@ -8,6 +8,7 @@ var solveSudoku = function (board) {
     if (DBG) console.log("INPUT:");
     if (DBG) printMatrix(board);
 
+    // Method to generate the ID for each box.
     function getBoxKey(x, y) {
         let box_X = x / 3;
         let box_Y = y / 3;
@@ -16,10 +17,13 @@ var solveSudoku = function (board) {
     }
 
     function fill(board, x, y) {
+
+        // If y == 9; it is out of range, so we assume it has reached the end of the board
         if (y == 9) {
             return true;
         }
 
+        // Calculate next position.
         let next_X = (x + 1) % 9;
         let next_Y = (next_X == 0) ? y + 1 : y;
 
@@ -27,52 +31,61 @@ var solveSudoku = function (board) {
             return fill(board, next_X, next_Y);
         }
 
+        // Try to fill numbers
         for (let i = 1; i <= 9; i++) {
             let boxKey = getBoxKey(x, y);
-            if (!rows[y][i] && !cols[x][i] && !boxes[boxKey][i]) {
-                rows[y][i] = 1;
-                cols[x][i] = 1;
-                boxes[boxKey][i] = 1;
-                board[y][x] = i;
+            if (!rows[y][i] &&      // There is no number "i" in the row.
+                !cols[x][i] &&      // There is no number "i" in the column.
+                !boxes[boxKey][i]   // There is no number "i" in the box.
+            ) {
+                rows[y][i] = 1; // Mark number "i" in the row has been taken.
+                cols[x][i] = 1; // Mark number "i" in the column has been taken.
+                boxes[boxKey][i] = 1; // Mark number "i" in the box has been taken.
+                board[y][x] = i; // Assign number "i" to the position.
                 if (fill(board, next_X, next_Y)) {
                     return true;
+                } else {
+                    // Rollback the number taken
+                    board[y][x] = '.';
+                    boxes[boxKey][i] = 0;
+                    cols[x][i] = 0;
+                    rows[y][i] = 0;
                 }
-                board[y][x] = '.';
-                boxes[boxKey][i] = 0;
-                cols[x][i] = 0;
-                rows[y][i] = 0;
             }
         }
         return false;
     }
 
-    // 
-    let rows = [];
-    let cols = [];
-    let boxes = [];
+    let rows = [];  // if 1 was taken in row #1, so mark rows[0][1] = 1;
+    let cols = [];  // if 1 was taken in column #1, so mark cols[0][1] = 1;
+    let boxes = []; // if 1 was taken in Box #1, so mark boxes[1][1] = 1;
 
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            let num = board[i][j];
-            let boxKey = getBoxKey(j, i);
-            if (boxes[boxKey] == null) {
-                boxes[boxKey] = [];
-            }
-            if (num == '.') {
-                continue;
-            }
-            if (rows[i] == null) {
-                rows[i] = [];
-            }
-            rows[i][num] = 1;
+    function cleanupMarkers() {
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                let num = board[i][j];
+                let boxKey = getBoxKey(j, i);
+                if (boxes[boxKey] == null) {
+                    boxes[boxKey] = [];
+                }
+                if (num == '.') {
+                    continue;
+                }
+                if (rows[i] == null) {
+                    rows[i] = [];
+                }
+                rows[i][num] = 1;
 
-            if (cols[j] == null) {
-                cols[j] = [];
+                if (cols[j] == null) {
+                    cols[j] = [];
+                }
+                cols[j][num] = 1;
+                boxes[boxKey][num] = 1;
             }
-            cols[j][num] = 1;
-            boxes[boxKey][num] = 1;
         }
-    }
+    };
+    cleanupMarkers();
+
     fill(board, 0, 0);
     if (DBG) console.log("OUTPUT");
     printMatrix(board);
