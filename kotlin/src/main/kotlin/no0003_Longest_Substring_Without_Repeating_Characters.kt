9bebdb2ce9689a941b11fe7bf419f5e880a75175
BH_Lin <binghuanlin@gmail.com>
2022-01-06 20:16:55 +0800

    Update no0003_Longest_Substring_Without_Repeating_Characters.kt

--- kotlin/src/main/kotlin/no0003_Longest_Substring_Without_Repeating_Characters.kt
+++ kotlin/src/main/kotlin/no0003_Longest_Substring_Without_Repeating_Characters.kt
@@ -1,44 +1,42 @@
-fun main(args: Array<String>) {
+fun main() {
 
     class Solution {
         fun lengthOfLongestSubstring(s: String): Int {
-            println("## INPUT: $s");
-            var temp = "";
-            var ans = "";
-            for (index in 0..s.length - 1) {
+            println("## INPUT: $s")
+            var temp = ""
+            var ans = ""
+            for (char in s) {
 
-                val char = s[index];
+                val pos = temp.indexOf(char)
 
-                val pos = temp.indexOf(char);
+                println("check $char in $temp --> $pos")
 
-                println("check $char in $temp --> $pos");
-
-                temp = temp.plus(char);
+                temp = temp.plus(char)
 
                 if (pos == -1) {
-                    println("temp => $temp");
+                    println("temp => $temp")
                 } else {
-                    print("replace temp from $temp");
-                    temp = temp.substring(pos + 1);
-                    println(" to $temp");
+                    print("replace temp from $temp")
+                    temp = temp.substring(pos + 1)
+                    println(" to $temp")
                 }
 
                 if (temp.length > ans.length) {
-                    ans = temp;
-                    println("current maxLength of substring = $ans");
+                    ans = temp
+                    println("current maxLength of substring = $ans")
                 }
             }
             println("## OUTPUT: $ans")
-            return ans.length;
+            return ans.length
         }
     }
 
-    println(">> main");
+    println(">> main")
 
-    val solution = Solution();
+    val solution = Solution()
 
-    val s = "abcabcbb";
+    val s = "abcabcbb"
 
-    solution.lengthOfLongestSubstring(s);
+    solution.lengthOfLongestSubstring(s)
 }
 
