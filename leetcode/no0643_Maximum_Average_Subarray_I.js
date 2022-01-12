7979e39eb4db433162c8bec45248db3ae841683a
BH_Lin <binghuanlin@gmail.com>
2022-01-12 22:40:45 +0800

    Update no0643_Maximum_Average_Subarray_I.js

--- leetcode/no0643_Maximum_Average_Subarray_I.js
+++ leetcode/no0643_Maximum_Average_Subarray_I.js
@@ -8,16 +8,12 @@ var findMaxAverage = function (nums, k) {
     let sum = 0;
     let tempArray = [];
     for (let i = 0; i < nums.length; i++) {
-
         let num = nums[i];
-        if (tempArray.length < k) {
-            tempArray.push(num);
-            sum += num;
-        } else if (tempArray.length >= k) {
+        if (tempArray.length >= k) {
             sum -= tempArray.shift();
-            tempArray.push(num);
-            sum += num;
         }
+        tempArray.push(num);
+        sum += num;
         if (tempArray.length == k) {
             if (sum / k > maxAverage) {
                 maxAverage = sum / k;
