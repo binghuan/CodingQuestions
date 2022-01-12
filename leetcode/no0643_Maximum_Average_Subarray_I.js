f9bc51d1e9dbd7a47cf792e7796cae22c74b5632
BH_Lin <binghuanlin@gmail.com>
2022-01-12 22:38:44 +0800

    Create no0643_Maximum_Average_Subarray_I.js

---
+++ leetcode/no0643_Maximum_Average_Subarray_I.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+    let maxAverage = Number.MIN_SAFE_INTEGER;
+    let sum = 0;
+    let tempArray = [];
+    for (let i = 0; i < nums.length; i++) {
+
+        let num = nums[i];
+        if (tempArray.length < k) {
+            tempArray.push(num);
+            sum += num;
+        } else if (tempArray.length >= k) {
+            sum -= tempArray.shift();
+            tempArray.push(num);
+            sum += num;
+        }
+        if (tempArray.length == k) {
+            if (sum / k > maxAverage) {
+                maxAverage = sum / k;
+            }
+        }
+    }
+    return maxAverage;
+};
