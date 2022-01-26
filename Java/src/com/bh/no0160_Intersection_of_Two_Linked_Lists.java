5c98c26fb9685d09b7f1d8194df7ca53ed9a0aaa
BH_Lin <binghuanlin@gmail.com>
2022-01-26 17:13:23 +0800

    add new coding question

---
+++ Java/src/com/bh/no0160_Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,43 @@
+package com.bh;
+
+import com.bh.no0206_Reverse_Linked_List.ListNode;
+
+import java.util.HashSet;
+
+public class no0160_Intersection_of_Two_Linked_Lists {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) {
+     * val = x;
+     * next = null;
+     * }
+     * }
+     */
+    static
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            HashSet<ListNode> nodes = new HashSet<ListNode>();
+            while (headA != null) {
+                nodes.add(headA);
+                headA = headA.next;
+            }
+
+            while (headB != null) {
+                if (nodes.contains(headB)) {
+                    return headB;
+                }
+
+                headB = headB.next;
+            }
+
+            return null;
+
+        }
+    }
+
+
+}
