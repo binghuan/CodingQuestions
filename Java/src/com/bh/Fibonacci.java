9678ef81bc043decebdf00c630811d320bbb7fdb
BH_Lin <binghuanlin@gmail.com>
2025-08-08 02:59:02 +0800

    Optimize Fibonacci with memoization
    Refactored the Fibonacci function to use a HashMap for memoization, significantly improving performance for large input values. Updated the main method to use the new memoized function.

--- Java/src/com/bh/Fibonacci.java
+++ Java/src/com/bh/Fibonacci.java
@@ -1,16 +1,31 @@
 package com.bh;
 
+import java.util.HashMap;
+
 public class Fibonacci {
-    public static long fibonacci(int n) {
-        if (n <= 1) return n;
-        else return fibonacci(n-1) + fibonacci(n-2);
+    public static Integer fibonacci(int n, HashMap<Integer, Integer> memo) {
+
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+
+        if (n <= 1) {
+            return n;
+        } else {
+            Integer value = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+            memo.put(n, value); // store the computed value in the hashmap
+            return value;
+        }
     }
 
     public static void main(String[] args) {
         int n = 100;
-        for (int i = 1; i <= n; i++)
-            System.out.println(i + ": " + fibonacci(i));
-    }
 
+        // hashmap to store computed Fibonacci numbers
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + ": " + fibonacci(i, memo));
+        }
+    }
 }
 
