e5cbee2aaec6c5bea95680134f7db45a0639c816
BH_Lin <binghuanlin@gmail.com>
2025-08-23 22:34:31 +0800

    Add explanatory comments to merge strings function
    Added detailed comments to clarify the logic of alternating character appends and handling remaining suffixes in the mergeStringsAlternately method.

--- Java/src/com/bh/no1768_Merge_Strings_Alternately.java
+++ Java/src/com/bh/no1768_Merge_Strings_Alternately.java
@@ -7,12 +7,15 @@ public class no1768_Merge_Strings_Alternately {
         int n1 = word1.length();
         int n2 = word2.length();
         int i = 0;
+        // Iterate while both still have characters at position i (交替追加兩字串當前索引的字元)
         while (i < n1 && i < n2) {
-            sb.append(word1.charAt(i));
-            sb.append(word2.charAt(i));
-            i++;
+            sb.append(word1.charAt(i)); // append character from word1 第 i 個字元
+            sb.append(word2.charAt(i)); // append character from word2 第 i 個字元
+            i++; // move to next index 前進索引
         }
+        // If word1 is longer, append its remaining suffix (若 word1 較長，追加剩餘部分)
         if (i < n1) sb.append(word1.substring(i));
+        // If word2 is longer, append its remaining suffix (若 word2 較長，追加剩餘部分)
         if (i < n2) sb.append(word2.substring(i));
         return sb.toString();
     }
