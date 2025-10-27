0d4a9192265750ea1fbda7e70ebab0c58c62ea91
BH_Lin <binghuanlin@gmail.com>
2025-10-27 20:19:40 +0800

    Add script to find top 20 largest numbers in file
    Introduces top20.js, which reads numbers from a file and outputs the top 20 largest values. Uses readline for efficient line-by-line processing.

---
+++ top20.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import readline from "readline";
+
+async function findTop20(filePath) {
+  const nums = [];
+
+  const fileStream = fs.createReadStream(filePath);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) {
+    const num = Number(line.trim());
+    if (isNaN(num)) continue;
+
+    if (nums.length < 20) {
+      nums.push(num);
+    } else {
+      const min = Math.min(...nums);
+      if (num > min) {
+        nums[nums.indexOf(min)] = num;
+      }
+    }
+  }
+
+  nums.sort((a, b) => b - a);
+  console.log("Top 20 largest numbers:", nums);
+}
+
+findTop20("fileA.txt");
